d917d1d90f3781c27c1504f29d0cc2963a7341ad
doron pagot <doronpagot@gmail.com>
2013-09-13 21:13:01 +0300

    refactor - remove unnecessary code

--- index.js
+++ index.js
@@ -4,16 +4,11 @@
 	module.exports = datify;
 
 	function datify(raw){
-		if (typeof raw === 'string'){
-			return datifyString(raw);
-		}
-		else {
-			return Traverse.map(raw, function(value){
-				if (typeof value === 'string'){
-					this.update(datifyString(value));
-				}
-			});
-		}
+		return Traverse.map(raw, function(value){
+			if (typeof value === 'string'){
+				this.update(datifyString(value));
+			}
+		});
 	};	
 
 	function datifyString(raw){
